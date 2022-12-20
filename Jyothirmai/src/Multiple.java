7678d58b1d747ff945a06668aee5b8b4e299999f
jyothi082001 <jyothimai082001@gmail.com>
2022-12-20 20:37:49 +0530

    new commit

---
+++ Jyothirmai/src/Multiple.java
@@ -0,0 +1,26 @@
+public class Multiple {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.eat();
+        a.travel();
+    }
+}
+interface AnimalEat{
+    void eat();
+}
+interface AnimalTravel{
+    void travel();
+}
+class Animal implements AnimalEat,AnimalTravel{
+    @Override
+    public void travel(){
+        System.out.println("Animal is travelling");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Animal is eating");
+        System.out.println("It is eating ");
+    }
+}
+
